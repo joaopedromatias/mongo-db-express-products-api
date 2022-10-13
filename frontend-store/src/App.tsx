9029f963fa44b0051509bdcdacb89c94d4c45ffe
joaopedromatias <joaopedromatias@outlook.com>
2022-10-13 12:56:05 -0300

    install frontend dependencies

--- frontend-store/src/App.tsx
+++ frontend-store/src/App.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // const [data, setData] = useState<Products[] | null>(null)
+
+  useEffect(() => { 
+    
+  }, [])
+
   return (
-    <div className="App">
-      
-    </div>
+    <Wrapper>
+      <div className='grid'>
+
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+margin: 0;
+font-size: 16px;
+.grid { 
+  display: grid;
+  /*grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(1fr, 300px);*/
+}
+`
 
 export default App;
