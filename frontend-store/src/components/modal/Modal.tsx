c09fcec362538daa5168e666e56b8f4d2061fc89
joaopedromatias <joaopedromatias@outlook.com>
2022-10-15 17:38:58 -0300

    style: modal style

--- frontend-store/src/components/modal/Modal.tsx
+++ frontend-store/src/components/modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import styled from "styled-components"
 
 interface WrapperProps { 
@@ -8,10 +9,27 @@ interface Props {
     type: 'UPDATE' | 'ADD' | 'DELETE' | null
     setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
     productName: string
+    productId: number
 }
 
-export const Modal: React.FC<Props> = ( { type, setIsModalOpen, productName }): JSX.Element => { 
+export const Modal: React.FC<Props> = ( { type, setIsModalOpen, productName, productId }): JSX.Element => { 
     
+    const handleDelete = (productId: number) => { 
+        
+    }
+
+    const modalCloseClick = ( { target }: MouseEvent ) => { 
+        if ((target as HTMLElement).classList[0] === 'dark-bg') { 
+            setIsModalOpen(false);
+        }
+    }
+
+    document.addEventListener('click', modalCloseClick);
+    
+    useEffect(() => { 
+        return () => document.removeEventListener('click', modalCloseClick) 
+    })
+
     var documentHeight:number = Math.max(
         window.document.body.scrollHeight,
         window.document.body.offsetHeight, 
@@ -20,35 +38,74 @@ export const Modal: React.FC<Props> = ( { type, setIsModalOpen, productName }): 
         window.document.documentElement.offsetHeight );   
     
     return <Wrapper documentHeight={documentHeight}>
+        <div className="dark-bg"></div>
         <div className="modal-base">
-                {type === 'UPDATE' ? 
-                <div className="modal"> 
-                    
-                </div>
-                : type === 'DELETE' ? 
-                <div className="modal">
-                    Do you really want to delete the {productName}?
-                </div>
-                : <></>}
+            {type === 'UPDATE' ? 
+            <div className="modal"> 
+                {/* UPDATE MODAL HERE */}
             </div>
+            : type === 'DELETE' ? 
+            <div className="modal">
+                <div className="modal-text">Are you sure you want to delete the {productName}?</div>
+                <button className="btn delete" onClick={() => handleDelete(productId)}>Delete</button>
+                <button className="btn cancel" onClick={() => setIsModalOpen(false)}>Cancel</button>
+            </div>
+            : type === 'ADD' ?
+            <div className="modal"> 
+                {/* ADD MODAL HERE */}
+            </div>
+            : <></>}
+        </div>
     </Wrapper>
 }
 
 const Wrapper = styled.div<WrapperProps>`
-    .modal-base { 
+    .dark-bg { 
         height: ${({ documentHeight }) => documentHeight+`px`};
         overflow: hidden;
-        overflow-y: hidden;
-        overflow-X: hidden;
         position: absolute;
         top: 0;
         left: 0;
         width: 100vw;
         opacity: 0.55;
         background-color: #4e4e4e;
+    }
+    .modal-base { 
+        position: absolute;
+        top: 0;
+        left: 0;
         .modal { 
-            opacity: 1;
-            background-color: white
+        margin: 0 calc(50vw - 225px);
+        opacity: 1;
+        position: fixed;
+        top: 40%;
+        background-color: #ececec;
+        padding: 25px 15px;
+        border-radius: 10px;
+        .modal-text { 
+            font-size: 1.3rem;
+        }
+        .btn { 
+            padding: 10px 15px;
+            margin: 15px 10px;
+            margin-bottom: 5px;
+            border-radius: 10px;
+            font-size: 1rem;
+            cursor: pointer;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            letter-spacing: 1.2px;
+        }
+        .delete { 
+            background-color: #e43b3b;
+            color: white;
+            border: 1px solid lightcoral;
+        }
+        .cancel { 
+            border: 0.5px solid #333533;
+            background-color: #ececec;
+            
         }
     }
+    }
+    
 `
--- frontend-store/src/components/store/ProductCard.tsx
+++ frontend-store/src/components/store/ProductCard.tsx
@@ -33,7 +33,7 @@ export const ProductCard: React.FC<Product> = ({name, id, image_url, price}): JS
             <EditIcon onclick={handleEdit}/>
             <DeleteIcon onclick={handleDelete}/>
         </div>
-        {isModalOpen ? <Modal type={modalType} setIsModalOpen={setIsModalOpen} productName={name}/> : <></>}
+        {isModalOpen ? <Modal type={modalType} setIsModalOpen={setIsModalOpen} productName={name} productId={id}/> : <></>}
     </Wrapper>
 }
 
